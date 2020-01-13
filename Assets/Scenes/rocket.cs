0ce30249eb496cc16e6e8868f3ff1034a0ce9f56
nananoizy <morihuelacazorla@gmail.com>
2020-01-13 13:41:23 +0100

    Thrust sound added

---
+++ Assets/Scenes/rocket.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour
+{
+
+    Rigidbody rigidBody;
+    AudioSource audioSource; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        ProcessInput();
+
+    }
+
+    private void ProcessInput(){
+
+        if (Input.GetKey(KeyCode.Space)){
+
+            //si no fuera relative, no giraria en up local sino global
+            rigidBody.AddRelativeForce(Vector3.up);
+
+            if (!audioSource.isPlaying)
+                audioSource.Play();
+        }
+        else
+            audioSource.Stop();
+
+        if (Input.GetKey(KeyCode.A)){
+            
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D)){
+            
+            transform.Rotate(-Vector3.forward);
+
+        }
+    }
+}
